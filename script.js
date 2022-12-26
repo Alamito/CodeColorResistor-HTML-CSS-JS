94ef989a8990047236f06b939824e7d6fa4e6243
Alamito <alamirbobroski12@gmail.com>
2022-12-26 16:22:17 -0300

    feat - add math of colors resistor

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// const colors = {
-//     black: '',
-//     brown: '',
-//     red: '',
-//     orange: '',
-//     yellow: '',
-//     green: '',
-//     blue: '',
-//     violet: '',
-//     gray: '',
-//     white: '',
-//     silver: '',
-//     gold: '',
-// }
-
 const linesColors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 const ToleranceColor = ['brown', 'red', 'green', 'blue', 'violet', 'grey', 'gold', 'silver'];
 
@@ -21,31 +6,28 @@ const lineMid = document.querySelector('.lineMid');
 const lineRight = document.querySelector('.lineRight');
 const lineTolerance = document.querySelector('.lineTolerance');
 
-let indexLineLeft = 0;
+let indexLineLeft = 1;
 let indexLineMid = 0;
-let indexLineRight = 0;
-let indexLineTolerance = 0;
+let indexLineRight = 2;
+let indexLineTolerance = 6;
+
+const valueTolerance = [1, 2, 0.5, 0.25, 0.10, 0.05, 5, 10];
 
 document.addEventListener('click', (click) => {
     if (click.target == lineLeft) {
-        lineLeft.src = `img/colors/lineLeft/${linesColors[indexLineLeft]}.png`;
         indexLineLeft = addIndexLine(indexLineLeft, 9);
+        lineLeft.src = `img/colors/lineLeft/${linesColors[indexLineLeft]}.png`;
     } else if (click.target == lineMid) {
-        lineMid.src = `img/colors/lineMid/${linesColors[indexLineMid]}.png`;
         indexLineMid = addIndexLine(indexLineMid, 9);
+        lineMid.src = `img/colors/lineMid/${linesColors[indexLineMid]}.png`;
     } else if (click.target == lineRight) { 
-        lineRight.src = `img/colors/lineRight/${linesColors[indexLineRight]}.png`;
         indexLineRight = addIndexLine(indexLineRight, 9);
+        lineRight.src = `img/colors/lineRight/${linesColors[indexLineRight]}.png`;
     } else if (click.target == lineTolerance) {
+        indexLineTolerance = addIndexLine(indexLineTolerance, 7);
         lineTolerance.src = `img/colors/lineTolerance/${ToleranceColor[indexLineTolerance]}.png`;
-        indexLineTolerance = addIndexLine(indexLineTolerance, 7)
     }
-
-    // if (indexLineTolerance === 7) {
-    //     indexLineTolerance = 0;
-    // } else {
-    //     indexLineTolerance++;
-    // }
+    mathOhms(indexLineLeft, indexLineMid, indexLineRight, indexLineTolerance, valueTolerance);
 });
 
 function addIndexLine(Indexline, maxIndex) {
@@ -56,4 +38,23 @@ function addIndexLine(Indexline, maxIndex) {
     }
     return Indexline;
 }
+
+function mathOhms (lineLeft, lineMid, lineRight, indexLineTolerance, valueTolerance) {
+    let ohms = `${lineLeft}${lineMid}`;
+    let multiply = '1';
+
+    for (let i = 0; i < lineRight; i++) { 
+        multiply = multiply + '0';
+    }
+
+    const lineTolerance = valueTolerance[indexLineTolerance];
+    
+    ohms = `${parseInt(ohms) * parseInt(multiply)} ± ${lineTolerance}%`;
+
+    console.log(ohms);
+}
+
+// function checkTolerance(indexLineTolerance) {
+//     return valueTolerance[indexLineTolerance];
+// }
 
