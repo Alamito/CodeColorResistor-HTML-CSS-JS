ae69c2b1a7fdaa2f76b77dfa9f0b13d5ee6421dd
Alamito <alamirbobroski12@gmail.com>
2022-12-27 16:13:16 -0300

    fix - bug notation

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-const linesColors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+const lineColorsLeftMid = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+const lineColorsRight = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'gold', 'silver'];
 const ToleranceColor = ['brown', 'red', 'green', 'blue', 'violet', 'grey', 'gold', 'silver'];
 
 const lineLeft = document.querySelector('.lineLeft');
@@ -18,13 +19,13 @@ const valueTolerance = [1, 2, 0.5, 0.25, 0.10, 0.05, 5, 10];
 document.addEventListener('click', (click) => {
     if (click.target == lineLeft) {
         indexLineLeft = addIndexLine(indexLineLeft, 9);
-        lineLeft.src = `img/colors/lineLeft/${linesColors[indexLineLeft]}.png`;
+        lineLeft.src = `img/colors/lineLeft/${lineColorsLeftMid[indexLineLeft]}.png`;
     } else if (click.target == lineMid) {
         indexLineMid = addIndexLine(indexLineMid, 9);
-        lineMid.src = `img/colors/lineMid/${linesColors[indexLineMid]}.png`;
+        lineMid.src = `img/colors/lineMid/${lineColorsLeftMid[indexLineMid]}.png`;
     } else if (click.target == lineRight) { 
         indexLineRight = addIndexLine(indexLineRight, 9);
-        lineRight.src = `img/colors/lineRight/${linesColors[indexLineRight]}.png`;
+        lineRight.src = `img/colors/lineRight/${lineColorsRight[indexLineRight]}.png`;
     } else if (click.target == lineTolerance) {
         indexLineTolerance = addIndexLine(indexLineTolerance, 7);
         lineTolerance.src = `img/colors/lineTolerance/${ToleranceColor[indexLineTolerance]}.png`;
@@ -43,53 +44,48 @@ function addIndexLine(Indexline, maxIndex) {
 
 function mathOhms (lineLeft, lineMid, lineRight, indexLineTolerance, valueTolerance) {
     let ohms = `${lineLeft}${lineMid}`;
-    let multiply = '1';
-
-    for (let i = 0; i < lineRight; i++) { 
-        multiply = multiply + '0';
-    }
+    let multiply = 1;
 
     const lineTolerance = valueTolerance[indexLineTolerance];
-    
-    ohms = notation(ohms, multiply, lineTolerance);
 
-    valueOhms.innerHTML = ohms; 
-}
-
-function notation(ohms, multiply, lineTolerance) { 
-    ohms = `${parseInt(ohms) * parseInt(multiply)}`;
-    
-    if (ohms.length <= 3) {
-        ohms = ohms + ' Ω'
-    }
-    else if (ohms.length >= 4 && ohms.length <= 7) {
-        ohms = ohms.split('');
-        ohms.splice(2, 2);
-        ohms = ohms.join('') + ' KΩ';
-
-        const index = ohms.length - 4;
-        ohms = ohms.substr(0, index) + ',' + ohms.substr(index);
-
-        if (ohms[ohms.length - 4] === '0') {
-            ohms = ohms.split('');
-            ohms.splice(ohms.length - 5, 2);
-            ohms = ohms.join('');
-        }
-
+    if (lineRight === 8) {
+        multiply = '0.1';
+        ohms = `${(parseFloat(ohms) * parseFloat(multiply)).toFixed(2)} ± ${lineTolerance}%`;
+        valueOhms.innerHTML = ohms; 
+    } else if (lineRight === 9) {
+        multiply = '0.01';
+        ohms = `${(parseFloat(ohms) * parseFloat(multiply)).toFixed(2)} ± ${lineTolerance}%`;
+        valueOhms.innerHTML = ohms; 
     } else {
-        ohms = ohms.split('');
-        ohms.splice(2, 6);
-        ohms = ohms.join('') + ' MΩ';
-
-        const index = ohms.length - 4;
-        ohms = ohms.substr(0, index) + ',' + ohms.substr(index);
-
-        if (ohms[ohms.length - 4] === '0') {
-            ohms = ohms.split('');
-            ohms.splice(ohms.length - 5, 2);
-            ohms = ohms.join('');
+        for (let i = 0; i < lineRight; i++) { 
+            multiply = multiply * 10;
         }
+        valueOhms.innerHTML = `${notation(ohms, multiply)} ± ${lineTolerance}%`; 
     }
-    return `${ohms} ± ${lineTolerance}%`;
+    
 }
 
+function notation(ohms, multiply) {
+    let temp;
+    if (multiply >= 100 && multiply < 100000) {
+        temp = `${parseInt(ohms) * parseFloat(multiply / 1000)}`;
+        if (temp.length > 3) {
+            temp = truncNumber(temp);
+        }
+        return `${temp} KΩ`
+    } else if (multiply >= 100000) {
+        temp = `${parseInt(ohms) * parseFloat(multiply / 1000000)} MΩ`;
+        if (temp.length > 3) {
+            temp = truncNumber(temp);
+        }
+        return `${temp} MΩ`
+    } else {    
+        return `${parseInt(ohms) * parseInt(multiply)} Ω`;
+    }
+}
+
+function truncNumber(temp) {
+    temp = temp.split('');
+    temp.splice(3, temp.length - 3);
+    return temp.join('');
+}
